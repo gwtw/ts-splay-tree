453219bcb6fb99f775bfb23a88ac22ef18a28e61
Daniel Imms <daimms@microsoft.com>
2018-05-19 10:17:45 -0700

    Add remove test

---
+++ src/test/remove.test.ts
@@ -0,0 +1,17 @@
+import { assert } from 'chai';
+import { TestSplayTree } from './testUtils';
+
+describe('add', () => {
+  it('should return the size of the tree', () => {
+    const tree = new TestSplayTree();
+    tree.add(1);
+    tree.add(2);
+    tree.add(3);
+    tree.add(4);
+    tree.add(5);
+    assert.equal(tree.size, 5);
+    tree.remove(3);
+    tree.remove(5);
+    assert.equal(tree.size, 3);
+  });
+});
